913533568cab71a21b16d9246284a142d430284c
xlight <xbluelight@gmail.com>
2016-03-03 02:26:07 +0800

    fix env
    Convert environment variables in the conf to fixed entries

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
-PORT=${BACKEND_PORT:-80}
-MEM=${CACHE_SIZE:-256m}
-sed -i 's|.port = "80"|.port = "${PORT}"|g'  /etc/varnish/default.vcl
-/usr/sbin/varnishd -F -P /run/.pid -a :6081 -T localhost:6082 -f /etc/varnish/default.vcl -S /etc/varnish/secret -s malloc,${MEM}
+
+# Convert environment variables in the conf to fixed entries
+# http://stackoverflow.com/questions/21056450/how-to-inject-environment-variables-in-varnish-configuration
+
+for name in BACKEND_PORT
+do
+    eval value=\$$name
+    sed -i "s|.port = \"80\"|.port = ${value}|g" /etc/varnish/default.vcl
+done
+
+/usr/sbin/varnishd -F -P /run/.pid -a :6081 -T localhost:6082 -f /etc/varnish/default.vcl -S /etc/varnish/secret -s malloc,${CACHE_SIZE}
