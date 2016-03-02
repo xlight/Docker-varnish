5cd6a09be90ffca1990888260814c1a0220a949d
xlight <xbluelight@gmail.com>
2016-03-03 02:59:05 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@
 for name in BACKEND_PORT
 do
     eval value=\$$name
-    sed -i "s|.port = \"80\"|.port = ${value}|g" /etc/varnish/default.vcl
+    sed -i "s|.port = \"80\"|.port = \"${value}\"|g" /etc/varnish/default.vcl
 done
 
 /usr/sbin/varnishd -F -P /run/.pid -a :6081 -T localhost:6082 -f /etc/varnish/default.vcl -S /etc/varnish/secret -s malloc,${CACHE_SIZE}
